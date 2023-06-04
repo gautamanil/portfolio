1b9e91c54b3a366c3adb9772f9bdc7bed007eda3
gautamanil <gautamanil@hotmail.com>
2023-06-04 01:16:48 +0000

    git-slice:ab7eb737a05fd8db3d7fbfa57de51aa745f8a325

---
+++ script.js
@@ -0,0 +1,31 @@
+const person1 = {
+  fullName: "Anil Gautam",
+  title: "Full-Stack Developer",
+  social: ["facebook", "instagram", "twitter", "linkedin", "youtube", "dribble"],
+  age: 25,
+  residence: "BD",
+  freelance: true,
+  address: "Kathmandu, Nepal",
+  languages: { Nepali: 100, English: 90, Hindi: 60 },
+  skills: { HTML: 90, CSS: 85, JavaScript: 80, React: 75, Express: 85 },
+  extraSkills: ["Tailwind", "SASS", "Python", "Flask", "GIT Knowledge"],
+};
+
+// const fullNameE = window.document.getElementById("fullName");
+// fullNameE.innerText = person1.fullName;
+// console.log(`executing...`);
+
+// const titleE = window.document.getElementById("title");
+// titleE.innerText = person1.title;
+
+// const ageE = window.document.getElementById("age");
+// ageE.innerText = person1.age;
+
+// const residenceE = window.document.getElementById("residence");
+// residenceE.innerText = person1.residence;
+
+// const freelanceE = window.document.getElementById("freelance");
+// freelanceE.innerText = person1.freelance ? `Available` : `Not Available`;
+
+// const addressE = window.document.getElementById("address");
+// addressE.innerText = person1.address;
