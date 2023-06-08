ee185f99647c305a0f2a14356ef8744dc610e94d
gautamanil <gautamanil@hotmail.com>
2023-06-08 01:07:32 +0000

    git-slice:24037aac56cb844b58c99118207c4ea2649cb4ac

--- script.js
+++ script.js
@@ -1,31 +1,107 @@
-const person1 = {
-  fullName: "Anil Gautam",
-  title: "Full-Stack Developer",
-  social: ["facebook", "instagram", "twitter", "linkedin", "youtube", "dribble"],
-  age: 25,
-  residence: "BD",
-  freelance: true,
-  address: "Kathmandu, Nepal",
-  languages: { Nepali: 100, English: 90, Hindi: 60 },
-  skills: { HTML: 90, CSS: 85, JavaScript: 80, React: 75, Express: 85 },
-  extraSkills: ["Tailwind", "SASS", "Python", "Flask", "GIT Knowledge"],
-};
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    const extData = JSON.parse(this.responseText);
+    addElements(extData);
+    addLanguages(extData);
+    addSkills(extData);
+    addExtraSkills(extData);
+  };
+  xhttp.open("GET", "info.json");
+  xhttp.send();
+}
 
-// const fullNameE = window.document.getElementById("fullName");
-// fullNameE.innerText = person1.fullName;
-// console.log(`executing...`);
+loadDoc();
 
-// const titleE = window.document.getElementById("title");
-// titleE.innerText = person1.title;
+function addElements(extData) {
+  // Name
+  const fullNameE = document.getElementById("fullName");
+  fullNameE.innerText = extData.fullName;
 
-// const ageE = window.document.getElementById("age");
-// ageE.innerText = person1.age;
+  // Title
+  const titleE = document.getElementById("title");
+  titleE.innerText = extData.title;
 
-// const residenceE = window.document.getElementById("residence");
-// residenceE.innerText = person1.residence;
+  // Social Media Images
+  const socialE = document.getElementById("social");
+  socialE.innerHTML = "";
+  for (let x in extData.social) {
+    const imgLink = extData.social[x].imageLink;
+    const para = document.createElement("div");
+    para.innerHTML = `<img class="w-6 h-6 rounded-[12px]" src='${imgLink}' />`;
+    socialE.appendChild(para);
+  }
 
-// const freelanceE = window.document.getElementById("freelance");
-// freelanceE.innerText = person1.freelance ? `Available` : `Not Available`;
+  // Age
+  const ageE = document.getElementById("age");
+  ageE.innerText = extData.age;
 
-// const addressE = window.document.getElementById("address");
-// addressE.innerText = person1.address;
+  // Residence
+  const residenceE = document.getElementById("residence");
+  residenceE.innerText = extData.residence;
+
+  // Freelance
+  const freelanceE = document.getElementById("freelance");
+  freelanceE.innerText = extData.freelance ? `Available` : `Not Available`;
+
+  // Address
+  const addressE = document.getElementById("address");
+  addressE.innerText = extData.address;
+}
+
+function addLanguages(extData) {
+  // Languages
+  const languageDivE = document.getElementById("language-div");
+  for (let x in extData.languages) {
+    // For Language = xx%
+    const para1 = document.createElement("div");
+    para1.setAttribute("class", "language mb-1 flex flex-row justify-between");
+    para1.innerHTML = `<p class="text-base text-klr_para">${extData.languages[x].name}</p>
+          <p class="text-base text-klr_para">${extData.languages[x].skill}%</p>`;
+    languageDivE.appendChild(para1);
+
+    // For Percentage Bar
+    const para2 = document.createElement("div");
+    para2.setAttribute("class", "language-bar mb-2.5");
+    para2.innerHTML = `
+      <div class="h-[9px] w-full border-[0.5px] border-solid border-klr_yellow rounded-[5px]">
+        <div class="h-[2px] border-[2px] border-solid border-klr_yellow bg-klr_yellow rounded-[2px] w-[${extData.languages[x].skill}%] m-[2px]"></div>
+      </div>`;
+    languageDivE.appendChild(para2);
+  }
+}
+
+function addSkills(extData) {
+  // Skills
+  const skillsDivE = document.getElementById("skill-div");
+  for (let x in extData.skills) {
+    // For HTML = 50%
+    const para1 = document.createElement("div");
+    para1.setAttribute("class", "skill mb-px flex flex-row justify-between");
+    para1.innerHTML = `<p class="text-base text-klr_para">${extData.skills[x].name}</p>
+          <p class="text-base text-klr_para">${extData.skills[x].skill}%</p>`;
+    skillsDivE.appendChild(para1);
+
+    // For Percentage Bar
+    const para2 = document.createElement("div");
+    para2.setAttribute("class", "skill-bar mb-2.5");
+    para2.innerHTML = `
+      <div class="h-[9px] w-full border-[0.5px] border-solid border-klr_yellow rounded-[5px]">
+        <div class="h-[2px] border-[2px] border-solid border-klr_yellow bg-klr_yellow rounded-[2px] w-[${extData.skills[x].skill}%] m-[2px]"></div>
+      </div>`;
+    skillsDivE.appendChild(para2);
+  }
+}
+
+function addExtraSkills(extData) {
+  // Skills
+  const extraSkillsDivE = document.getElementById("extra-skill-div");
+  for (let x in extData.extraSkills) {
+    // For Extra Skills
+    const para = document.createElement("div");
+    para.setAttribute("class", "mb-4 flex flex-row gap-4");
+    para.innerHTML = `<img class="w-5 h-5" src="images/double_square.jpg" />
+            <p>${extData.extraSkills[x]}</p>`;
+    extraSkillsDivE.appendChild(para);
+  }
+}
